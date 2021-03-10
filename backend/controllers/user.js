27953a875890fd79225873a0acf6c24889041d95
Joshua Brionne <Joshua.brionne@gmail.com>
2021-03-10 13:08:51 +0100

    adding comment

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -12,9 +12,11 @@ exports.signup = (req, res, next) => {
      const username = req.body.username
      const password = req.body.password
 
-     const regexEmail = /^[a-z0-9._-ç]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/
+     const regexEmail = /^[a-z0-9._-ç]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/ // regexes the check the inputs
      const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{3,}$/
 
+
+     // if forest to check once again the inputs
      if (!email[0] || !username[0] || !password[0]) {
           return res.status(400).json({
                error: 'Champs manquant'
@@ -43,7 +45,7 @@ exports.signup = (req, res, next) => {
           })
      }
 
-     models.User.findOne({
+     models.User.findOne({ // checking if the user does not already exist
                attributes: ['email', 'username', 'role'],
                where: {
                     [Op.or]: [{
@@ -56,8 +58,8 @@ exports.signup = (req, res, next) => {
                }
           })
           .then(userFound => {
-               if (!userFound) {
-                    bcrypt.hash(password, 10)
+               if (!userFound) { // if the user isn't found, create it 
+                    bcrypt.hash(password, 10) // hashing the infos
                          .then(hash => {
                               const user = models.User.create({ // Creating the user
                                         email: email,
@@ -70,7 +72,7 @@ exports.signup = (req, res, next) => {
                                         res.status(201).json({
                                              userId: user.id,
                                              role: user.role,
-                                             token: jwt.sign({
+                                             token: jwt.sign({ // creating the JWT 
                                                        userId: user.id
                                                   },
                                                   process.env.JWT_SECRET, {
@@ -108,7 +110,7 @@ exports.login = (req, res, next) => {
           })
      }
 
-     models.User.findOne({
+     models.User.findOne({ //getting the user linked with the email
                attributes: ['email', 'password', 'role', 'id'],
                where: {
                     email: email
@@ -116,14 +118,14 @@ exports.login = (req, res, next) => {
           })
           .then(user => {
                if (user) {
-                    bcrypt.compare(password, user.password)
+                    bcrypt.compare(password, user.password) // compare the passwords
                          .then(valid => {
                               if (!valid) {
-                                   return res.status(406).json({
+                                   return res.status(406).json({ // if it does not match
                                         error: 'Email ou mot de passe incorrect!'
                                    })
                               }
-                              res.status(200).json({
+                              res.status(200).json({ // then
                                    userId: user.id,
                                    role: user.role,
                                    token: jwt.sign({
@@ -151,19 +153,19 @@ exports.login = (req, res, next) => {
 
 exports.deleteUser = (req, res, next) => {
      const token = req.headers.authorization.split(' ')[1]
-     const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+     const decodedToken = jwt.verify(token, process.env.JWT_SECRET) // getting token
      const userId = decodedToken.userId
 
      const elem = req.body.body
 
-     models.User.findOne({
+     models.User.findOne({ // getting the user
                attributes: ['id', 'role', 'username'],
                where: {
                     id: req.params.id
                }
           })
           .then(user => {
-               if (user.id === userId && elem == user.username) {
+               if (user.id === userId && elem == user.username) { // if the confirmation is right and the user is deleting himself
                     return user.destroy()
                          .then(() => res.status(200).json({
                               message: 'Utilisateur supprimé !'
@@ -174,19 +176,19 @@ exports.deleteUser = (req, res, next) => {
                               })
                          });
                }
-               models.User.findOne({
+               models.User.findOne({ // if the user.id isn't matching, check if the user is an admin
                          attributes: ['id', 'role'],
                          where: {
                               id: userId
                          }
                     })
                     .then(userAdmin => {
-                         if (userAdmin.role != true) {
+                         if (userAdmin.role != true) { // if not
                               return res.status(406).json({
                                    error: 'Un problème est survenue lors de la supprésion de l\'utilisateur.'
                               })
                          }
-                         user.destroy()
+                         user.destroy() //then
                               .then(() => res.status(200).json({
                                    message: 'Utilisateur supprimé !'
                               }))
@@ -208,13 +210,14 @@ exports.deleteUser = (req, res, next) => {
 
 exports.updateUser = (req, res, next) => {
      const token = req.headers.authorization.split(' ')[1]
-     const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+     const decodedToken = jwt.verify(token, process.env.JWT_SECRET) // getting token
      const userId = decodedToken.userId
 
      const body = req.body.body
-     const value = req.body.value
+     const value = req.body.value // value will be the variable to define with modification we want to do
      parseInt(value, 10);
 
+     // checking the values
      const regexEmail = /^[a-z0-9._-ç]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/
      const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{3,}$/
 
@@ -231,7 +234,7 @@ exports.updateUser = (req, res, next) => {
                error: 'Mot de passe trop court ou incorrecte!'
           })
      }
-     if (value == 1) {
+     if (value == 1) { // modifying the username
           models.User.findOne({
                     attributes: ['username'],
                     where: {
@@ -239,7 +242,7 @@ exports.updateUser = (req, res, next) => {
                     }
                })
                .then(userFound => {
-                    if (!userFound) {
+                    if (!userFound) { // if the new username isn't already taken
                          models.User.findOne({
                                    attributes: ['id', 'role', 'username'],
                                    where: {
@@ -247,12 +250,12 @@ exports.updateUser = (req, res, next) => {
                                    }
                               })
                               .then(user => {
-                                   if (body == user.username) {
+                                   if (body == user.username) { // if it is the same
                                         return res.status(406).json({
                                              error: 'Pas de modification'
                                         })
                                    }
-                                   if (user.id == userId) {
+                                   if (user.id == userId) { // modify
                                         if (body.length < 3) {
                                              return res.status(406).json({
                                                   error: 'pseudo trop court !'
@@ -268,18 +271,18 @@ exports.updateUser = (req, res, next) => {
                                                   error: 'Une erreur est survenue lors de la modification'
                                              }));
                                    }
-                                   models.User.findOne({
+                                   models.User.findOne({ // checking if he is an admin
                                              attributes: ['id', 'role'],
                                              where: {
                                                   id: userId
                                              }
                                         })
                                         .then(userAdmin => {
-                                             if (userAdmin.role != true) {
+                                             if (userAdmin.role != true) { // if not
                                                   return res.status(406).json({
                                                        error: 'Impossible de modifier cet utilisateur.'
                                                   })
-                                             } else if (userAdmin.role == 1) {
+                                             } else if (userAdmin.role == 1) { // then
                                                   user.update({
                                                             username: body
                                                        })
@@ -308,7 +311,7 @@ exports.updateUser = (req, res, next) => {
                     error
                }))
      }
-     if (value == 2) {
+     if (value == 2) { // modifying the email
           models.User.findOne({
                     attributes: ['email'],
                     where: {
@@ -316,7 +319,7 @@ exports.updateUser = (req, res, next) => {
                     }
                })
                .then(userFound => {
-                    if (!userFound) {
+                    if (!userFound) { // if the new email isn'ty already taken
                          models.User.findOne({
                                    attributes: ['id', 'role', 'username'],
                                    where: {
@@ -329,7 +332,7 @@ exports.updateUser = (req, res, next) => {
                                              error: 'Pas de modification'
                                         })
                                    }
-                                   if (user.id == userId) {
+                                   if (user.id == userId) { // if the user is the owner of the account
                                         if (body.length < 3) {
                                              return res.status(406).json({
                                                   error: 'pseudo trop court !'
@@ -351,7 +354,7 @@ exports.updateUser = (req, res, next) => {
                                                   id: userId
                                              }
                                         })
-                                        .then(userAdmin => {
+                                        .then(userAdmin => { // if not, check if he is an admin
                                              if (userAdmin.role != true) {
                                                   return res.status(406).json({
                                                        error: 'Impossible de modifier cet utilisateur.'
@@ -385,7 +388,7 @@ exports.updateUser = (req, res, next) => {
                     error
                }))
      }
-     if (value == 3) {
+     if (value == 3) { // modifying the password
           models.User.findOne({
                     attributes: ['id', 'role', 'username'],
                     where: {
@@ -393,10 +396,10 @@ exports.updateUser = (req, res, next) => {
                     }
                })
                .then(user => {
-                    bcrypt.hash(body, 10)
+                    bcrypt.hash(body, 10) // hash the new password
                          .then(hash => {
-                              const tmp = hash // Creating the user
-                              if (user.id == userId) {
+                              const tmp = hash
+                              if (user.id == userId) { // if the user is the owner of the account
                                    return user.update({
                                              password: tmp
                                         })
@@ -407,18 +410,18 @@ exports.updateUser = (req, res, next) => {
                                              error: 'Une erreur est survenue lors de la modification'
                                         }));
                               }
-                              models.User.findOne({
+                              models.User.findOne({ // if not, check if he is an admin
                                         attributes: ['id', 'role'],
                                         where: {
                                              id: userId
                                         }
                                    })
                                    .then(userAdmin => {
-                                        if (userAdmin.role != true) {
+                                        if (userAdmin.role != true) { // if not
                                              return res.status(406).json({
                                                   error: 'Impossible de modifier cet utilisateur.'
                                              })
-                                        } else if (userAdmin.role == 1) {
+                                        } else if (userAdmin.role == 1) { // then
                                              user.update({
                                                        password: tmp
                                                   })
@@ -447,7 +450,7 @@ exports.updateUser = (req, res, next) => {
 
 
 exports.getUser = (req, res, next) => {
-     models.User.findOne({
+     models.User.findOne({ // getting all the informations linked with a user related to the req.params.id
                attributes: ['id', 'username', 'email'],
                where: {
                     id: req.params.id
@@ -470,11 +473,10 @@ exports.getUser = (req, res, next) => {
           })
 }
 
-
+// routes that will unlink all the medias linked with a user if he deletes his account
 exports.destroyUserMedias = (req, res, next) => {
 
      let i = -1;
-     console.log(req.body.body)
      while (req.body.body[++i]) {
           if (req.body.body) {
                const filename = req.body.body[i].split('/images/')[1]; // deleting the linked file
