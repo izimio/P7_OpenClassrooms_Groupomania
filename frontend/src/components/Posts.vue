bac0b433e6d514b8337dadabeb850dc5fd8664ac
Joshua Brionne <Joshua.brionne@gmail.com>
2021-03-12 17:11:18 +0100

    fix height

--- frontend/src/components/Posts.vue
+++ frontend/src/components/Posts.vue
@@ -121,6 +121,7 @@ $bg-red: #501b1d;
     -ms-flex-align: center;
     align-items: center;
     background-color: lighten($bg-red, 40);
+    padding: 0.5em;
     img {
       width: 400px;
       height: 300px;
@@ -140,6 +141,7 @@ $bg-red: #501b1d;
     -ms-flex-align: center;
     align-items: center;
     background-color: lighten($bg-red, 40);
+    padding: 0.5em;
     img {
       width: 700px;
       height: 500px;
